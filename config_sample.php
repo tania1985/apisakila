7c7a13def4f575966e0ed80b0aaacdc3d7939f5c
tania1985 <testevezbautista@danielcastelao.org>
2025-02-13 18:02:54 +0100

    verificando el token y generar

--- config_sample.php
+++ config_sample.php
@@ -3,4 +3,5 @@ define(constant_name: "HOST", value: "dirección del servidor");
 define(constant_name:"DB_USER", value:"usuario bbdd");
 define(constant_name:"DB_PASS", value:"password bbdd");
 define(constant_name:"DB_DATABASE", value:"bbdd");
+define("JWT_SECRET_KEY", "tu_clave_secreta");
 ?>
